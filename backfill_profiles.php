6b32fba951468544a6f405235c2c49f00f744b3f
unknown <edgardollentas04@gmail.com>
2026-06-15 18:39:56 +0800

    chore: remove one-off backfill_profiles script

--- backfill_profiles.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'config/config.php';
-$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-$db->exec('INSERT IGNORE INTO user_profiles (user_id) SELECT id FROM users');
-echo 'Done';
